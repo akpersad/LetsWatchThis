5734ccdc074438b1aa6d2a85fb379737ea408b99
Andrew Persad <akpersad@gmail.com>
2021-02-11 01:10:04 -0500

    React router implemented

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import "./styles/main.scss";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import AppContainer from "./components/appContainer/appContainer";
@@ -7,7 +8,10 @@ class App extends Component {
   render() {
     return (
       <>
-        <AppContainer />
+        <Switch>
+          <Route path="/" exact component={AppContainer} />
+          <Route path="/something" component={AppContainer} />
+        </Switch>
       </>
     );
   }
