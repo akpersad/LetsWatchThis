7b34470df05bfc3fb13d5cb9e6aaeab160b3c102
Andrew Persad <akpersad@gmail.com>
2021-02-14 00:03:14 -0500

    Copied all from tutorial

--- server.js
+++ server.js
@@ -3,15 +3,31 @@ const express = require("express");
 const path = require("path");
 const mysql = require("mysql");
 const netflixDB = require("./expressFiles/netflixDB");
+const dbDetails = require("./serverApp/config/db.config");
+const db = require("./serverApp/models");
 
+const Role = db.role;
 const app = express();
-const dbDetails = {
-	host: process.env.MYSQL_HOST,
-	user: process.env.MYSQL_USER,
-	password: process.env.MYSQL_PWD,
-	database: process.env.MYSQL_DB
-};
 const pool = mysql.createPool(dbDetails);
+const initial = () => {
+	Role.create({
+		id: 1,
+		name: "user"
+	});
+
+	Role.create({
+		id: 2,
+		name: "moderator"
+	});
+
+	Role.create({
+		id: 3,
+		name: "admin"
+	});
+};
+
+require("./serverApp/routes/auth.routes")(app);
+require("./serverApp/routes/user.routes")(app);
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, "dist")));
@@ -41,4 +57,9 @@ app.get("*", (req, res) => {
 
 app.listen(port, function() {
 	console.log(`App is listening on port ${port}`);
+});
+
+db.sequelize.sync({ force: true }).then(() => {
+	console.log("Drop and Resync Db");
+	initial();
 });
