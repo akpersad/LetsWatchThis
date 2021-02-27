6a4157a2c46f2ac7be2c9b946bb088360d8fc12b
Andrew Persad <akpersad@gmail.com>
2021-02-26 21:43:39 -0500

    Profile Header

--- src/components/profileHeader/profileHeader.jsx
+++ src/components/profileHeader/profileHeader.jsx
@@ -1,10 +1,48 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { AvatarGenerator } from "random-avatar-generator";
+import { Link } from "react-router-dom";
 // import PropTypes from "prop-types";
 
 class ProfileHeader extends Component {
+  constructor() {
+    super();
+    this.state = {
+      imgSrc: ""
+    };
+  }
+
+  componentDidMount() {
+    const generator = new AvatarGenerator();
+
+    // Simply get a random avatar
+    this.setState({ imgSrc: generator.generateRandomAvatar() });
+  }
+
   render() {
-    return <div>ProfileHeader</div>;
+    const { imgSrc } = this.state;
+    return (
+      <div className="profile-header-container container">
+        <div className="image-container">
+          <img className="profile-image" src={imgSrc} alt="Profile Logo " />
+        </div>
+        <div className="info-container">
+          <div className="name-line">
+            <h3 className="name-header">Andrew Persad</h3>
+          </div>
+          <div className="email-line">
+            <h4 className="email-header">akpersad@gmail.com</h4>
+          </div>
+          <div className="link-line">
+            <h5 className="link-header">
+              <Link className="link" to="/choices">
+                <span data-content="What to watch?">What to watch?</span>
+              </Link>
+            </h5>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
