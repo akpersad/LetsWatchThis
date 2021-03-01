888a3ecb31b59cc3cb5c45fea42a9d17d91b3868
Andrew Persad <akpersad@gmail.com>
2021-03-01 01:29:11 -0500

    Google Analytics Setup

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import GA from "./components/googleAnalytics/googleAnalytics";
 import App from "./app";
 import store from "./config/store";
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
+      {GA.init() && <GA.RouteTracker />}
       <App />
     </BrowserRouter>
   </Provider>,
